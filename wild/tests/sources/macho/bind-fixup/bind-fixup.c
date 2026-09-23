35e628702204eedd4f626f1f2ccf378160168ea4
Martin Liska <martin.liska@hey.com>
2026-09-23 08:44:22 +0200

    port(MachO): encode implicit addend in the bind fixups

--- wild/tests/sources/macho/bind-fixup/bind-fixup.c
+++ wild/tests/sources/macho/bind-fixup/bind-fixup.c
@@ -5,7 +5,16 @@
 //#ExpectSym:_ptr section="__data",segment="__DATA"
 
 extern int value;
+extern const unsigned char bytes[1024];
 
 int* volatile ptr = &value;
+const unsigned char* volatile ptr16 = bytes + 16;
+const unsigned char* volatile ptr255 = bytes + 255;
 
-int main(void) { return *ptr; }
+// TODO: requires DYLD_CHAINED_IMPORT_ADDEND fixup format
+// const unsigned char* volatile ptr1023 = bytes + 1023;
+
+int main(void) {
+  if (*ptr16 != 16 || *ptr255 != 255) return 1;
+  return *ptr;
+}
